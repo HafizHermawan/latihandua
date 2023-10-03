1cdcfa3d80ae15b2c025016f7c74e5831c8049ca
HafizHermawan <hafizhermawan45@gmail.com>
2023-10-03 09:57:12 +0700

    ditambahkan oleh hafizh

--- pustaka-booking/application/controllers/tugaspertemuan2.php
+++ pustaka-booking/application/controllers/tugaspertemuan2.php
@@ -3,7 +3,7 @@ class tugaspertemuan2 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1>perkenalan</h1>";
+        echo "<h1>kenalan</h1>";
         echo "Nama Saya Hafizh Hermawan saya tinggal di Jakarta Barat saya kuliah di UBSI dan saya memiliki hobby yaitu Sepakbola";
     }
-}
+}    
