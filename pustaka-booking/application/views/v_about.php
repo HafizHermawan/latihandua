91edad93588692c0992563320b647994e28ccaaa
HafizHermawan <hafizhermawan45@gmail.com>
2023-10-09 16:21:14 +0700

    telah diperbarui

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Hafizh</li>
+        <li>Nama Belakang : Hermawan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan KS TUBUN III NO 51 SLIPI JAKARTA BARAT</li>
+    </ul>
+
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>JAKARTA</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Sepakbola</li>
+    </ul>
+</section>
